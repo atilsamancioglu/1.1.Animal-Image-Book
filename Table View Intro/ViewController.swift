7579721aa5764e3ed713c310c424d89de199b0bf
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-10 16:43:05 +0300

    animal image book just completed

--- Table View Intro/ViewController.swift
+++ Table View Intro/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    var animalArray = [String]()
+    
+    var animalImageArray = [UIImage]()
+    
+    var animalchosen = ""
+    var imagechosen = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        //example array
+        
+        animalArray.append("Elephant")
+        animalArray.append("Polar Bear")
+        animalArray.append("Harp Seal")
+        animalArray.append("Dog")
+        animalArray.append("Cat")
+        animalArray.append("Bird")
+        animalArray.append("Tiger")
+        animalArray.append("Lion")
+        
+        animalImageArray.append(UIImage(named: "elephant.jpg")!)
+        animalImageArray.append(UIImage(named: "polarbear.jpg")!)
+        animalImageArray.append(UIImage(named: "harpseal.jpeg")!)
+        animalImageArray.append(UIImage(named: "dog.jpg")!)
+        animalImageArray.append(UIImage(named: "cat.jpg")!)
+        animalImageArray.append(UIImage(named: "bird.jpg")!)
+        animalImageArray.append(UIImage(named: "tiger.jpg")!)
+        animalImageArray.append(UIImage(named: "lion.jpg")!)
+
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = animalArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            animalArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+            
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.animalchosen = animalArray[indexPath.row]
+        self.imagechosen = animalImageArray[indexPath.row]
+        performSegue(withIdentifier: "imageViewSegue", sender: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "imageViewSegue" {
+            let destinationVC = segue.destination as! imageViewController
+            destinationVC.selectedAnimal = self.animalchosen
+            destinationVC.selectedImage = self.imagechosen
+            
+        }
     }
-
-
+ 
 }
 
--- Table View Intro/imageViewController.swift
+++ Table View Intro/imageViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class imageViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var animalLabel: UILabel!
+    
+    var selectedAnimal = ""
+    var selectedImage = UIImage()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        imageView.image = selectedImage
+        animalLabel.text = selectedAnimal
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+   
 }
