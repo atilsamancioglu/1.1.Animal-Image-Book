d7cdfd27c557164123dcc7bb05b3a408d245f8b8
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-10 13:36:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 1.1.Animal-Image-Book
---
+++ Table View Intro/imageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  imageViewController.swift
+//  Table View Intro
+//
+//  Created by Atıl Samancıoğlu on 10/12/2016.
+//  Copyright © 2016 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
